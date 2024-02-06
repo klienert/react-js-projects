d347a35cb47d9c6f157866a6ca17f357b9d9104f
Keith <klienert@madisoncollege.edu>
2024-02-05 22:05:07 -0600

    begining first componenet

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
+// https://youtu.be/5ZdHfJVAY-s?si=AJhaXd5c4CbPokLd
+
 import './App.css';
+import Accordian from './components/accordian';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* Accordian component */}
+      <Accordian/>
+    
+      {/* Tic Tac Toe */}
+      {/* <Tic-tac-toe/> */}
+
+
+
     </div>
   );
 }
