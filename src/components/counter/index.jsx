a2309b171d61be712debd67c082e5d4b0fd55cfe
Keith <klienert@esc6.net>
2024-02-09 10:47:46 -0600

    updated counter

--- src/components/counter/index.jsx
+++ src/components/counter/index.jsx
@@ -1,22 +1,70 @@
-import React from "react";
+import React, { useState } from "react";
 
-export default class Counter extends React.Component {
+export default function Counter() {
 
-    constructor (props) {
-        console.log('Constructor');
-        super(props)
-        this.state = {
-            counter: 0
-        }
+    // const [count, setCount] = useState(0);
+    
+    // try with a function in useState()
+    // const [count, setCount] = useState(() => {
+    //     console.log("run function");
+    //     return 4
+    // })
+
+    // try with passing an object (*Notice the change in other functions...)
+    // const [state, setState] = useState({count: 4, theme: 'blue'})
+    // const count = state.count;
+    // const theme = state.theme;
+
+    // const incCount = () => {
+    //     setState(prevState => {
+    //         return { 
+    //             ...prevState,
+    //             count: prevState.count + 1                
+    //         }
+    //     })            
+    // }
+
+    // const decCount = () => {
+    //     setState(prevState => {
+    //         return { 
+    //             ...prevState,
+    //             count: prevState.count - 1
+    //         }
+    //     })
+    // }
+
+    // could also have multiple useState()
+    const [count, setCount] = useState(4);
+    const [theme, setTheme] = useState('');
+    
+
+    const incCount = () => {
+        setCount(prevCount => prevCount + 1)
     }
 
-    render () {
-        console.log('render');
-        return 
-        <div>
-            <div className="counter">
-                Counter: {this.state.counter}
-            </div>
-        </div>
+    const decCount = () => {
+        setCount(prevCount => prevCount - 1)
     }
+
+    
+
+    const spanClass = () => {
+        let res = '';
+        if (count > 0) {
+            res = 'text-success p-1';
+        } else if (count < 0) {
+            res = 'text-danger p-1';
+        } else {
+            res = 'text-primary p-1';
+        }        
+        return res;
+    }
+
+    return <div className="container">
+
+        <button onClick={decCount}> - </button>
+        <span className={spanClass()}>{count}</span>        
+        <button onClick={incCount}> + </button>
+        
+    </div>
 }
