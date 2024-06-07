4ad600b94a413862b1cf916a82e8564d9aee6e29
Keith <klienert@madisoncollege.edu>
2024-06-07 15:51:59 -0500

    added google charts

---
+++ src/components/google_charts/index.js
@@ -0,0 +1,22 @@
+// https://www.weatherapi.com/my/
+// https://api.weatherapi.com/v1/forecast.json?key=3ccb52233b424b7a8d9140104232303&q=London&days=5
+const TEST = '77320';
+
+
+// substitute TEST for loc, bring loc in as a param
+const GoogleChart = () => {
+    
+
+    // console.log("Current Date from API: " + (data && data.forecast.forecastday[0].date));
+    // console.log("1st Forecast Date from API: " + (data && data.forecast.forecastday[1].date));
+    // console.log("2nd Forecast Date from API: " + (data && data.forecast.forecastday[2].date));
+
+
+    return (
+        <>
+        <h1>Test Google Chart component</h1>
+        </>        
+    )
+}
+
+export default GoogleChart;
