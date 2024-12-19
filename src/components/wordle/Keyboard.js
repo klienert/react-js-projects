e4b85f282e7f0c7004e2ab7d230e91ceed70d435
Keith <klienert@madisoncollege.edu>
2024-12-19 17:40:06 -0600

    fixed keyboard

--- src/components/wordle/Keyboard.js
+++ src/components/wordle/Keyboard.js
@@ -8,25 +8,40 @@ const Keyboard = () => {
         ["A","S","D","F","G","H","J","K","L"], 
         ["↵","Z","X","C","V","B","N","M","←"]
     ];
-
+    const keys1 = ["Q","W","E","R","T","Y","U","I","O","P"];
+    const keys2 =  ["A","S","D","F","G","H","J","K","L"];
+    const keys3 =  ["Z","X","C","V","B","N","M"];
     
 
     return (
         <div className="wordle-keyboard">
             <div className="line1">
-                {keys[0].map((key) => {
+                {/* {keys[0].map((key) => {
+                    return <Key keyVal={key}/>
+                })} */}
+                {keys1.map((key) => {
                     return <Key keyVal={key}/>
                 })}
             </div>
             <div className="line2">
-                {keys[1].map((key) => {
+                {/* {keys[1].map((key) => {
+                    return <Key keyVal={key}/>
+                })} */}
+                {keys2.map((key) => {
                     return <Key keyVal={key}/>
                 })}
             </div>
             <div className="line3">
-                {keys[2].map((key) => {
+                {/* {keys[2].map((key) => {
+                    return <Key keyVal={key}/>
+                })} */}
+                {/* Insert Enter Key */}
+                <div className="key" id="big">ENTER</div>
+                {keys3.map((key) => {
                     return <Key keyVal={key}/>
                 })}
+                {/* Insert Backspace Key */}
+                <div className="key" id="big">←</div>
             </div>
         </div>
     )
