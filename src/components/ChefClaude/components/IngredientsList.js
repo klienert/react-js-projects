760572e189ac20815fcd434cf12c6dfa53322ff7
Keith <klienert@esc6.net>
2025-01-15 13:26:40 -0600

    finished chef claude with API/AI integration

---
+++ src/components/ChefClaude/components/IngredientsList.js
@@ -0,0 +1,28 @@
+import React, { useState } from "react";
+
+const IngredientsList = ( { ing, showRecipe}) => {
+    
+    const ingredientListItems = ing.map(x => (
+        <li key={x}>{x}</li>
+    ))
+
+    return (
+        <section>
+            <h2>Ingredients on hand:</h2>
+            <ul className="ingredients-list" aria-live="polite">{ingredientListItems}</ul>
+            {ingredientListItems.length > 3 && 
+                <div className="get-recipe-container">
+                    <div>
+                        <h3>Ready for a Recipe?</h3>
+                        <p>Generate a recipe from yoru list of ingredients.</p>
+                    </div>
+                    <button
+                        onClick={showRecipe}
+                    >Get a Recipe</button>
+                </div>
+            }
+        </section>
+    )
+}
+
+export default IngredientsList;
---
+++ src/components/ChefClaude/components/api.js
@@ -0,0 +1,34 @@
+import { Anthropic } from '@anthropic-ai/sdk';
+
+const SYSTEM_PROMPT = `You are an assistant that receives a list of ingredients that a user has and suggests a recipe they could make with some or all of those ingredients. You do not need to use every ingredient they mention in your recipe. The recipe can include additional ingredients they did not mention, but try not to include too many extra ingredients. Format your response in markdown to make it easier to render to a web page.`;
+
+// verify api key is available
+const API_KEY = process.env.REACT_APP_ANTHROPIC_API_KEY;
+if (!API_KEY) {
+    console.error("No API Key found...");    
+}
+
+const anthropic = new Anthropic({
+    apiKey: API_KEY,
+    dangerouslyAllowBrowser: true,
+});
+
+export async function getRecipeFromChefClaude(ingredientsArr) {
+    // add a try/catch?
+    const ingredientsString = ingredientsArr.join(", ");
+    const msg = await anthropic.messages.create({
+        model: "claude-3-5-sonnet-20241022",
+        max_tokens: 1024,
+        messages: [
+            { 
+                role: "user", 
+                content: `I have ${ingredientsString}. Please give me a recipe you would recommend I make!`
+            }
+        ],
+        system: SYSTEM_PROMPT
+    });
+    // console.log(ingredientsString);
+    // console.log(msg);
+    return msg.content[0].text;
+}
+
